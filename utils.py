a13fe4102c4938de307caaba761cd8c1e16ca79f
zach <zoldham2@illinois.edu>
2020-02-29 07:17:17 -0600

    Added parameter utility functions

--- utils.py
+++ utils.py
@@ -6,10 +6,16 @@ TASKSET_TOKEN = "TASKSET"
 CLOSE_TOKEN = "CLOSE"
 SETUP_TOKEN = "SETUP"
 WORK_TOKEN = "WORK"
+RESULT_TOKEN = "RESULT"
 
 TASKDEF_PARAM = "TASKDEF"
 TASKLIST_PARAM = "TASKLIST"
 CLIENTID_PARAM = "CLIENTID"
+TASK_PARAM = "TASK"
+RESULT_PARAM = "RESULT"
+RESULTLIST_PARAM = "RESULTLIST"
+WORKERCOUNT_PARAM = "WORKERCOUNT"
+QUEUESIZE_PARAM = "QUEUESIZE"
 
 class TaskDef:
     def __init__(self, source_file, other_files, function):
@@ -22,3 +28,9 @@ def pack(obj):
 
 def unpack(obj_string):
     return pickle.loads(base64.b64decode(obj_string.encode("utf-8")))
+
+def buildParameter(name, value):
+    return '{n}:{v}'.format(n=name, v=value)
+
+def parseParameter(param):
+    return param.split(':')
