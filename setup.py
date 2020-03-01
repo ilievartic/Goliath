abb657f90e84162e32bae54a9771e0c2d41b0372
Logan Pulley <logan@pulley.host>
2020-03-01 07:14:07 -0600

    Updated for v0.1.2

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-        long_description = fh.read()
+    long_description = fh.read()
 
 setuptools.setup(
     name='goliath',  
-    version='0.1.1',
+    version='0.1.2',
     scripts=['goliath'] ,
     author="Ilie Vartic",
     author_email="ilie.vartic@gmail.com",
     description="This package enables python coders to build \"multi-threaded\" programs and optimize their data processing.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ilievartic/Goliath/archive/0.1.tar.gz",
+    url="https://github.com/ilievartic/Goliath/archive/v0.1.2.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
     ],
 )
