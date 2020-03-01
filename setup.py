25781ec2111f718fe64236fe4dc18cdd24d639df
Ilie Vartic <ilie.vartic@gmail.com>
2020-03-01 07:08:00 -0600

    updated info for version 0.1.1

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='goliath',  
-    version='0.1',
+    version='0.1.1',
     scripts=['goliath'] ,
     author="Ilie Vartic",
     author_email="ilie.vartic@gmail.com",
-    description="AAA",
+    description="This package enables python coders to build \"multi-threaded\" programs and optimize their data processing.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ilievartic/Goliath",
+    url="https://github.com/ilievartic/Goliath/archive/0.1.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
